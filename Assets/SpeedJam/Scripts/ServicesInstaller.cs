005d9552297cdf40ecd808172135758c7a3b9719
NeutrinoZh <neutrinozh@gmail.com>
2025-02-28 19:15:09 +0200

    project: setup

---
+++ Assets/SpeedJam/Scripts/ServicesInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace SpeedJam
+{
+    public class ServicesInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
