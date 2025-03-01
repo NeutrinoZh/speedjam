53d07759d32cee18ae39fcb4aa90fce03fcb9b10
Sanyo4ek373 <oleksandr.panchenko373@gmail.com>
2025-03-01 13:38:41 +0200

    Added fuel slider

---
+++ Assets/SpeedJam/Scripts/FuelSlider.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelSlider : MonoBehaviour
+{
+    [SerializeField] private int _maxFuelAmount;
+
+    private Slider _slider;
+
+    public void ChangeFuelAmount(int value)
+    {
+        _slider.value = value;
+    }
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+        _slider.maxValue = _maxFuelAmount;
+        _slider.value = _maxFuelAmount;
+    }
+}
